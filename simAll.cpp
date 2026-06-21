bdbc1d029257095108aa5190312ce303af2f6bbf
SHenPltk <shenpltk@gmail.com>
2026-06-21 11:21:56 +0800

    update linkPuzzle

--- simAll.cpp
+++ simAll.cpp
@@ -10,31 +10,20 @@ int main()
 	sim.addComponent("power", "E");
 	sim.addComponent("resistor", "R");
 	sim.addComponents("bulb", std::vector<std::string>{"L1", "L2"});
-	sim.addComponents("switch", std::vector<std::string>{"a", "b", "c", "d"});
+	sim.addComponents("switch", std::vector<std::string>{"a", "b", "c"});
 
-	sim.addLink("E.p", "R.l");
-	sim.addLink("R.r", "L1.l");
-	sim.addLink("L1.r", "c.l");
-	sim.addLink("c.r", "E.n");
-	sim.addLink("R.r", "d.l");
-	sim.addLink("d.r", "b.l");
-	sim.addLink("b.r", "L2.l");
-	sim.addLink("L2.r", "E.n");
-	sim.addLink("L1.r", "a.l");
-	sim.addLink("a.r", "d.r");
+	sim.linkCircle(std::vector<std::string>{"E","R","a","L1"});
+	sim.linkSeries("R.r",std::vector<std::string>{"b","c"},"E.p");
+	sim.addLink("a.r","L2.l");	sim.addLink("L2.r","c.l");
 
-	sim.addSwitchGroup("g", std::vector<std::string>{"d", "c", "b", "a"});
+	sim.addSwitchGroup("g", std::vector<std::string>{"c", "b", "a"});
 
-	for (int i = 0; i < 1 << 4; i++)
+	for (int i = 0; i < 1 << 3; i++)
 	{
 		sim.setSwitchGroupState("g", i);
-		auto results = sim.simulate();
-		for (const auto &result : results)
-		{
-			std::cout << result.state << ' ';
-		}
-		std::cout << std::endl;
+		std::cout << sim.formatOutput("@s @ | @b @\n");
 	}
 
+	std::cout << sim.toString();
 	return 0;
 }
